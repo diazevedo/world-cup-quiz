a909844495e0c8d8b1990a15d4642a8fa50d3d9d
Diego Azevedo <dapptn@hotmail.com>
2020-11-30 01:24:27 +1100

    first page

---
+++ js/app.js
@@ -0,0 +1,9 @@
+// M.toast({ html: "I am a toast!" });
+// $(document).ready(function () {
+//   M.updateTextFields();
+// });
+
+document.addEventListener("DOMContentLoaded", function () {
+  var elems = document.querySelectorAll(".sidenav");
+  var instances = M.Sidenav.init(elems, {});
+});
